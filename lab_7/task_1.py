d87626fe6fd36753304776af9a030cad06c24e6b
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2023-10-22 06:14:22 -0700

    lab7

--- lab_7/task_1.py
+++ lab_7/task_1.py
@@ -1,34 +1,68 @@
 class Employee:
-    def __init__(self, emp_number, emp_name, cnic, contact_phone, contact_address):
-        self.emp_number = emp_number
-        self.emp_name = emp_name
-        self.cnic = cnic
-        self.contact_phone = contact_phone
-        self.contact_address = contact_address
+    def __init__(self,employee_no,name,cnic_no,phone,address):
+        self.employeeNo = employee_no
+        self.name = name
+        self.cnicNo = cnic_no
+        self.phoneNo = phone
+        self.address = address
 
-
-class SalariedEmployee(Employee):
-    def __init__(self, emp_number, emp_name, cnic, contact_phone, contact_address, weekly_salary):
-        super().__init__(emp_number, emp_name, cnic, contact_phone, contact_address)
-        self.weekly_salary = weekly_salary
-
-    def calculate_pay(self):
-        return self.weekly_salary
-
-class HourlyEmployee(Employee):
-    def __init__(self, emp_number, emp_name, cnic, contact_phone, contact_address, hours_worked, rate_per_hour):
-        super().__init__(emp_number, emp_name, cnic, contact_phone, contact_address)
-        self.hours_worked = hours_worked
-        self.rate_per_hour = rate_per_hour
-
-    def calculate_pay(self):
-        if self.hours_worked <= 40:
-            return self.hours_worked * self.rate_per_hour
-        else:
-            regular_pay = 40 * self.rate_per_hour
-            overtime_pay = (self.hours_worked - 40) * (1.5 * self.rate_per_hour)
-            return regular_pay + overtime_pay
+    def __str__(self):
+        a = ""
+        a += f"\n{str(self.employeeNo)} "
+        a += f" {str(self.name)} "
+        a += f" {str(self.cnicNo)} "
+        a += f" {str(self.phoneNo)} " 
+        a += f" {str(self.address)} "
+        return a
     
+class Salaried(Employee):
+    def __init__(self,employee_no,name,cnic_no,phone,address,week_salary):
+        super().__init__(employee_no,name,cnic_no,phone,address)
+        self.weekSalary = week_salary
+
+    def calculatePay(self):
+        return self.weekSalary
+    
+    def __str__(self):
+        b = ""
+        b += super().__str__()
+        b += f"{str(self.weekSalary)}"
+        return b
+    
+class Hourly(Employee):
+    def __init__(self, employee_no, name, cnic_no, phone, address,paid_for_each_hour,hour_worked):
+        super().__init__(employee_no, name, cnic_no, phone, address)
+        self.paidForEachHour = paid_for_each_hour
+        self.hourWorked = hour_worked
+
+    def calculatePay(self):
+        if self.hourWorked<=40:
+            return (self.hourWorked*self.paidForEachHour)
+        else:
+            overTimeHours = self.hourWorked-40
+            overTimePay = (self.paidForEachHour*40)+(1.5*overTimeHours*self.paidForEachHour)
+            return overTimePay
+        
+    def __str__(self):
+        c = ""
+        c += super().__str__()
+        c += f"{str(self.paidForEachHour)}"
+        c += f"{str(self.hourWorked)}"
+        return c
+    
+class Commission(Employee):
+    def __init__(self, employee_no, name, cnic_no, phone, address,sales_amount):
+        super().__init__(employee_no, name, cnic_no, phone, address)
+        self.salesAmount = sales_amount
+
+    def calculatePay(self):
+        return (80/100)*self.salesAmount
+    
+    def __str__(self):
+        d = ""
+        d +=  super().__str__()
+        d += str(self.salesAmount)
+        return d
 
 class Payroll:
     def __init__(self):
@@ -37,26 +71,39 @@ class Payroll:
     def add(self,employee):
         self.Employee.append(employee)
 
-    def calculate_total_payroll(self):
-        total_payroll = 0
+    def totalAmount(self):
+        total = 0
         for employee in self.Employee:
-            total_payroll += employee.calculate_pay()
-        return total_payroll
+            total += employee.calculatePay()
+        return total
     
     def __str__(self):
-        for i in range(len(self.Employee)):
-            return f"{self.Employee[i]}"
+        e = ""
+        for employee in self.Employee:
+            e += f"{str(employee)}"
+        return e
+        
+            
+    
 def main():
     payroll = Payroll()
-    a = (HourlyEmployee("1","Kashaf","3108-6478632","0378-88736765","Johar Town Lahore",50,80))
-    b = (SalariedEmployee("1","Kashaf","3108-6478632","0378-88736765","Johar Town Lahore",8000))
-    payroll.add(a)
-    payroll.add(b)
+    employee1 = Salaried("24","Kashaf","31301-0000000000","0300-11111111","johar Town,Lahore",5000)
+    employee2 = Hourly("25","Hamna","31301-222222222","0300-55555555","G1,Lhaore",15,45)
+    employee3 = Hourly("26","Tehreem","31301-555554444","0300-99999999","Township,Lhaore",15,35)
+    employee4 = Commission("27","Rabbiya","31301-333888999","0300-000555773","DHA,Lhaore",15000)
+    employee5 = Salaried("29","Muqadsa","31301-27795555","0300-2675183","Gulburg,Lhaore",7000)
+    payroll.add(employee1)
+    payroll.add(employee2)
+    payroll.add(employee3)
+    payroll.add(employee4)
+    payroll.add(employee5)
     print(payroll)
+    total = payroll.totalAmount()
+    print(total)
 
-    
-
-
-
-    
 main()
+
+
+
+
+
